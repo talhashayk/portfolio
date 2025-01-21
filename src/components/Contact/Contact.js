7345cc6dfcf7fb4b703f9783101252d4f10fde11
talhashayk <talhashake@gmail.com>
2025-01-21 21:01:42 +0000

    utilise component data files

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,81 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Contact.scss";
-import githubIcon from "../../assets/github.svg";
-import linkedinIcon from "../../assets/linkedin.svg";
-import mailIcon from "../../assets/mail.svg";
+import { botMessagesData, userOptionsData } from "./contactData.js";
 
 const Contact = ({ setActiveSection }) => {
 	const [conversation, setConversation] = useState([]);
 
-	const botMessages = [
-		"Hi there! I'm Talha Bot.",
-		"I'd be thrilled to assist with any questions you may have.",
-		"How may I be of service?",
-	];
+	const botMessages = botMessagesData;
 
-	const [userOptions, setUserOptions] = useState({
-		"I just wanted to say hi!": [
-			"Hi to you too!",
-			"I hope you enjoy browsing.",
-			"Thanks for visiting!",
-			"Anything else I can help with?",
-		],
-		"Can I see your GitHub?": [
-			<a
-				href="https://github.com/talhashayk"
-				target="_blank"
-				rel="noopener noreferrer"
-				className="response-link"
-			>
-				<img src={githubIcon} alt="GitHub" className="response-icon" />
-				Here's my GitHub.
-			</a>,
-			"Feel free to browse my repositories!",
-			"Let me know if you need anything from there.",
-			"Happy coding!",
-		],
-		"Where can I contact you?": [
-			<a
-				href="https://www.linkedin.com/in/talhasheikhswe/"
-				target="_blank"
-				rel="noopener noreferrer"
-				className="response-link"
-			>
-				<img
-					src={linkedinIcon}
-					alt="LinkedIn"
-					className="response-icon"
-				/>
-				Message me on LinkedIn.
-			</a>,
-			<a
-				href="mailto:talhashake@gmail.com"
-				target="_blank"
-				rel="noopener noreferrer"
-				className="response-link"
-			>
-				<img src={mailIcon} alt="Email" className="response-icon" />
-				Email me
-			</a>,
-			"Looking forward to hearing from you!",
-			"I'll respond as soon as I can!",
-		],
-		"I'd like to hire you.": [
-			"That's awesome!",
-			<a
-				href="mailto:talhashake@gmail.com"
-				target="_blank"
-				rel="noopener noreferrer"
-				className="response-link"
-			>
-				<img src={mailIcon} alt="Email" className="response-icon" />
-				You can email me here.
-			</a>,
-			"I would love to hear about the project.",
-			"Feel free to share the details anytime.",
-			"Looking forward to working together!",
-		],
-	});
+	const [userOptions, setUserOptions] = useState(userOptionsData);
 
 	const handleOptionClick = (option) => {
 		setConversation((prevConversation) => [
@@ -152,7 +84,7 @@ const Contact = ({ setActiveSection }) => {
 				<div className="contact-box-photo-title">
 					<div className="contact-box-photo-wrapper">
 						<img
-							src={require("../../images/image2.png")}
+							src={require("../../images/image1.png")}
 							alt="Description"
 							className="contact-box-photo"
 						/>
--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -1,33 +1,11 @@
 import React, { useState } from "react";
 
 import "./ImageCarousel.scss";
-import image1 from "../../images/image1.png";
-import image2 from "../../images/image2.png";
-import image3 from "../../images/image3.png";
-import image4 from "../../images/image4.png";
-import image5 from "../../images/image5.png";
-import image6 from "../../images/image6.png";
-import image7 from "../../images/image7.png";
-import image8 from "../../images/image8.png";
-import image9 from "../../images/image9.png";
-import image10 from "../../images/image10.png";
-import image11 from "../../images/image11.png";
+import { imageCarouselData } from "./imageCarouselData";
 import expandIcon from "../../assets/expand.svg";
 
 const ImageCarousel = () => {
-	const images = [
-		{ image: image1, caption: "Catching rays" },
-		{ image: image2, caption: "Prague, Christmas Markets" },
-		{ image: image3, caption: "Catch and release" },
-		{ image: image4, caption: "I'm for lunch" },
-		{ image: image5, caption: "Livingstone" },
-		{ image: image6, caption: "Chiang Mai" },
-		{ image: image7, caption: "Koh Samui, Night Markets" },
-		{ image: image8, caption: "Jellyfish Museum" },
-		{ image: image9, caption: "Bouldering" },
-		{ image: image10, caption: "The Algarve" },
-		{ image: image11, caption: "Koh Tao" },
-	];
+	const images = imageCarouselData;
 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
