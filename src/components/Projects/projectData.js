035cc1c948e02f4a6755d743c851dee63743a000
talhashayk <talhashake@gmail.com>
2025-01-21 21:24:48 +0000

    update project titles

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,6 +1,6 @@
 export const projectData = [
 	{
-		title: "Component Tech Stack Migration",
+		title: "Legacy to Modern Migration",
 		description:
 			"Oversaw and assisted two contractors in migrating a component from Java, Handlebars, and Vanilla JavaScript to a modern React and Node.js framework. This effort simplified and improved the codebase, aligning it with the latest technologies and best practices. Provided domain knowledge and supported backend integration throughout the process.",
 		skills: [
@@ -15,7 +15,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "SendGrid Integration",
+		title: "SendGrid Email Automation",
 		description:
 			"Automated Preloved newsletter sign-ups by integrating with SendGrid, a third-party email client, to reduce manual workload for cross-functional teams. Developed the new service using Java 21 and SpringBoot to manage this process efficiently.",
 		skills: [
@@ -28,7 +28,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Ezoic Integration",
+		title: "Ezoic Ads Proxy Setup",
 		description:
 			"Integrated Ezoic, a third-party ad provider, as a proxy for Preloved using CNAMEs to enhance display ad revenue. Adapted the frontend to ensure proper ad display and functionality. Conducted performance tests to assess the impact of the integration and continuously optimized it for improved results.",
 		skills: [
@@ -52,19 +52,19 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Search Optimizations",
+		title: "Predictive Search Optimisation",
 		description:
 			"Optimized Preloved’s search functionality using ElasticSearch and SpringBoot (Java), implementing a feature to aggregate and store type-ahead suggestions. This enhancement led to a nearly 40% increase in user engagement, specifically among users upgrading to respond to ads.",
 		skills: [
 			"ElasticSearch",
 			"Spring Boot",
-			"Search Performance Optimization",
+			"Search Performance Optimisation",
 			"Data Aggregation",
 			"User Engagement Metrics",
 		],
 	},
 	{
-		title: "GitHub Migration",
+		title: "GitLab to GitHub Migration",
 		description:
 			"Contributed to migrating all Preloved components from GitLab to GitHub, modifying repositories to secure sensitive information across various tech stacks. Ensured GitHub repositories were fully compatible with GoCD for seamless application deployment.",
 		skills: [
@@ -76,7 +76,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Display Ad Creation",
+		title: "Display Ad Development",
 		description:
 			"Developed and integrated new display ads using Google AdSense for Preloved, focusing on anchor ads. Successfully implemented these ads within the Preloved frontend using React, Handlebars, and ColdFusion.",
 		skills: [
@@ -89,7 +89,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Archived Advert Mitigation",
+		title: "Archived Ads Resolution",
 		description:
 			"Resolved issues with ElasticSearch that caused archived customer ads to be incorrectly displayed on the platform, leading to inaccessible content. The resolution significantly improved the customer experience by eliminating false positives and ensuring only relevant ads were shown.",
 		skills: [
@@ -101,7 +101,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Line Management",
+		title: "Line Management and Mentorship",
 		description:
 			"Served as a line manager, mentoring junior engineers by developing their skills and guiding them through challenges. Collaborated with and indirectly managed contractors, offering support on company practices and domain knowledge to ensure timely and successful project outcomes.",
 		skills: [
@@ -112,7 +112,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Yoti Integration",
+		title: "Yoti ID Verification",
 		description:
 			"Integrated Yoti’s ID verification API into Preloved’s platform using ColdFusion, enhancing the existing manual process. This upgrade provided users with a quick and seamless authentication method, enabling them to access the platform's features earlier. Additionally, it reduced the workload for the customer service team by automating member verification.",
 		skills: [
@@ -123,7 +123,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Boost Membership Creation",
+		title: "Boost Membership Launch",
 		description:
 			"Developed the ‘Boost’ membership tier for Preloved from the ground up, using a ColdFusion backend. Designed and implemented the frontend for three components with ColdFusion, React, and Handlebars, and updated the database to support and monitor the new membership. Additionally, implemented tracking for conversions and user engagement by creating custom tags in Google Tag Manager and integrating them with Google Analytics.",
 		skills: [
@@ -136,7 +136,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Bidex Integration",
+		title: "Dealer Lookup Integration",
 		description:
 			"Integrated the front and backend of the Bidex dealership lookup service for Endura's German site using Java, collaborating with multiple teams across THG to ensure full functionality and availability for applicable products. Implemented modifiable content parameters to enable easy activation of the service for other sites and countries with configurable content.",
 		skills: [
@@ -148,7 +148,7 @@ export const projectData = [
 		],
 	},
 	{
-		title: "Documentation",
+		title: "Documentation Enhancement",
 		description:
 			"Led the enhancement of documentation for Preloved by using Confluence to thoroughly document and share domain knowledge. Streamlined the process for capturing new insights and changes, ensuring up-to-date documentation. Additionally, created multiple templates for Jira to improve ticket creation and task details for better clarity and efficiency.",
 		skills: [
